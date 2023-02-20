92b81679532da254efee7657e62eb49b92547bfe
rismandwij <rismandwijumansyah@gmail.com>
2023-02-21 06:52:25 +0700

    percobaan table jurnal

---
+++ table_jurnal.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS public.jurnal (
+    doi character varying(15) NOT NULL,
+    judul_paper character varying(50) NOT NULL,
+    tahun integer NOT NULL,
+    id_penulis character NOT NULL,
+    nama_penulis character varying(50) NOT NULL,
+    id_instansi character NOT NULL,
+    instansi character NOT NULL,
+    jumlah_sitasi integer,
+    id_penerbit character NOT NULL,
+    penerbit character NOT NULL,
+    volume integer,
+    PRIMARY KEY (doi) 
+)
