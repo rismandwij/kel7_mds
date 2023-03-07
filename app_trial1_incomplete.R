6c83e98053e7207b95a3a157f7514a4d75371e4f
Akmarina Khairunnisa <akmarinakhairunnisa@gmail.com>
2023-03-07 09:03:08 +0700

    Update app_trial1_incomplete.R
    Progress Front-End:
    1. Rencana platform sejauh ini akan terdiri dari 3 tab
        >< "View Tables" : Menampilkan tabel entitas dan tabel relasi dilengkapi fitur filter yang akan disesuaikan dengan tujuan platform
        >< "Performance Dashboard" : Menampilkan dashboard ranking score jurnal per instansi, per departemen, atau per kategori (masih belum fix)
        >< "About"  : Menjelaskan informasi terkait platform
    
    2. Tab "View Tables" sudah ditambahkan Dynamic UI "table intro" untuk menjelaskan deskripsi dari masing-masing table
    
    3. Beberapa penyesuaian visual dan penempatan pada body tab "View Tables"
    
    Target selanjutnya:
    
    1. Hubungkan sisi back-end
    2. Perbaikan visual agar lebih aksesibel
    3. Penambahan DynamicUI lainnya agar lebih interaktif
    4. Penguatan alur experience pengguna. Ada rencana penambahan tab Home, namun belum dipastikan apa yang akan menjadi kontennya

--- app_trial1_incomplete.R
+++ app_trial1_incomplete.R
@@ -11,18 +11,54 @@ ui <- dashboardPage(
     div(htmlOutput("Selamat Datang!"), style = "padding: 20px"),
     sidebarMenu(
       menuItem("View Tables", tabName = "view_table", icon = icon("search")),
-      menuItem("Create Tables", tabName = "create_table", icon = icon("plus-square")),
-      menuItem("Update Tables", tabName = "update_table", icon = icon("exchange-alt")),
-      menuItem("Insert Entries", tabName = "insert_value", icon = icon("edit")),
-      menuItem("Delete Tables", tabName = "del_table", icon = icon("trash-alt")),
       menuItem("Performance Dasboard", tabName = "performance", icon = icon("chart-simple")),
       menuItem("About", tabName = "about", icon = icon("info-circle"))
     )
   )
   ,
-  dashboardBody()
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "view_table", uiOutput("tab_1")),
+      tabItem(tabName = "about", uiOutput("tab_2"))
+    )
+  )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) {
+  output$tab_1 <- renderUI({
+    box(width = NULL, status = "primary",
+        sidebarLayout(
+          sidebarPanel(
+            box(width = 12,
+                collapsible = TRUE,
+                div(style = "height: 15px; background-color: white;"),
+                title = "Database Info:",
+                p("")),
+            selectInput(inputId = "sel_table_1",
+                        label = "Tables in Database",
+                        choices = dbListTables(db),
+                        selected = "penulis"),
+            textOutput(outputId = "tab_intro"),
+            tags$head(tags$style("#tab_intro{font-size: 15px;font-style: italic;}"))
+          ),
+          mainPanel(
+            h4(strong("Table Preview")),
+            dataTableOutput(outputId = "sel_table_view")
+          )
+        )
+    )
+  })
+  output$sel_table_view <- renderDataTable()
+  table_intro <- list(penulis = "Penjelasan tabel", 
+                      judul = "Penjelasan tabel",
+                      instansi = "Penjelasan tabel",
+                      departemen = "Penjelasan tabel")
+  output$tab_intro <- renderText(
+    if (input$sel_table_1 %in% c("penulis","judul","instansi","departemen"))
+    {table_intro[[input$sel_table_1]]}
+    else {table_intro$other}
+  )
+}
 
 shinyApp(ui, server)
+
